1a823d6393305cdfa7600447128e462790ccb6d6
codingPotato <adnan.youssef@ymail.com>
2024-05-25 15:32:38 +0400

    Update M3DatePicker
    - Updated M3DatePicker to show days outside the current selected month.
    - Updated M3DatePicker to show exactly 6 weeks in the calendar at all times.
    - Fixed theme override for the text color of days outside the current month.

--- src/Components/M3DatePicker/M3DatePicker.tsx
+++ src/Components/M3DatePicker/M3DatePicker.tsx
@@ -3,7 +3,7 @@ import { DatePicker, DatePickerProps } from '@mui/x-date-pickers';
 
 const M3DatePicker = (props: Omit<DatePickerProps<never, boolean>, 'children'> & { children?: React.ReactNode }) => {
   return (
-    <DatePicker {...props}/>
+    <DatePicker showDaysOutsideCurrentMonth fixedWeekNumber={6} {...props}/>
   );
 };
 
